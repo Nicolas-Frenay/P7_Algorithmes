c1c11581ca66571941a8c1a8a55f33abe624cb8e
MilhouseVanProuten <milhousevanprouten@gmail.com>
2021-11-06 17:26:21 +0100

    code cleaned and fully working

--- bruteforce.py
+++ bruteforce.py
@@ -1,7 +1,14 @@
 import csv
+from glob import glob
 
 
 def loading_data(file):
+    """
+    loading function
+    :param file: csv file name to analyse
+    :return: None, call knapsack function
+    """
+
     # reading *.csv file
     data = csv.DictReader(open(file))
 
@@ -18,10 +25,17 @@ def loading_data(file):
 
 
 def search_best_profit(data, max_budget):
+    """
+    function to find the best shares combination
+    :param data: list of shares : [name, price, profit]
+    :param max_budget: int, max value to spend.
+    :return: None, call display_best function
+    """
+
     budget_max = max_budget
     best_shares = []
 
-    # generating each unique combinaisons possible
+    # generating each unique combinations possible
     combinations = []
 
     # need an empty list in combinations for starting the powerset
@@ -29,16 +43,19 @@ def search_best_profit(data, max_budget):
         starter_list = []
         combinations.append(starter_list)
 
+    # creating for each share a copy of each combinations already in the list,
+    # and adding the new share on every copy. Then add those new combinations
+    # to the list
     for item in data:
-        sub_sets = [sub+[item] for sub in combinations]
+        sub_sets = [sub + [item] for sub in combinations]
         combinations.extend(sub_sets)
 
     # deleting empty list at index 0, to avoid iteration error in get price and
     # profit functions
     combinations.pop(0)
 
-    # checking for each combinaisons if cost is under 500, then check if profits
-    # is larger than actual best
+    # checking for each combinations if cost is under 500, then check if
+    # profits is larger than actual best
     for shares in combinations:
         if get_price(shares) <= budget_max:
             if get_profits(shares) > get_profits(best_shares):
@@ -47,6 +64,12 @@ def search_best_profit(data, max_budget):
 
 
 def get_price(data):
+    """
+    function to get the total cost of a set of shares
+    :param data: list of shares : [name, price, profit]
+    :return: float
+    """
+
     price = 0
     for share in data:
         price += share[1]
@@ -54,6 +77,12 @@ def get_price(data):
 
 
 def get_profits(data):
+    """
+    Function to get profits form a set of shares
+    :param data: list of shares : [name, price, profit]
+    :return: float
+    """
+
     profits = 0
     for share in data:
         profits += share[2]
@@ -61,19 +90,41 @@ def get_profits(data):
 
 
 def display_best(shares, budget):
+    """
+    function to display the best shares combination, their total cost, and
+    their profits
+    :param shares: list of shares : [name, price, profit]
+    :param budget: float, total cost of the shares
+    :return:  None, print the results
+    """
+
     total_profit = 0
     for share in shares:
         print(share[0])
         total_profit += share[2]
     rendement = (total_profit / budget) * 100
     print('\n Cout : {} €.'.format(budget))
-    print('\n Benefice total : {:.2f} €, rendement : {:.2f} %.'.format(
-        total_profit, rendement))
+    print('\n Benefice total : {:.2f} €,'
+          ' rendement : {:.2f} %.'.format(total_profit, rendement))
 
 
 # time for :
 # 20 shares :   2.54s    cost :     498.00€,    profits :     99.08€
 
-if __name__ == '__main__':
-    loading_data('shares.csv')
+def main():
+    """
+    Main function, user choose which file to analyse.
+    :return: None, call loading_data function
+    """
 
+    csv_files = glob("csv_files/*.csv")
+
+    for index, file in enumerate(csv_files):
+        print(index, file[10:])
+
+    sel = int(input("Entrez l'index du fichier à analyser : "))
+    loading_data(csv_files[sel])
+
+
+if __name__ == '__main__':
+    main()
